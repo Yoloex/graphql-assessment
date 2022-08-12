e043bb9eda8150b5cba6f6618427d20f53f64b02
Elsa <elsa@Elsas-MacBook-Pro.local>
2022-08-11 22:44:22 -0400

    Using custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DropBox from "./components/DropBox";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-function App() {
+const client = new ApolloClient({
+  uri: "http://prod-cart.chicoryapp.com/api/graph/",
+  cache: new InMemoryCache(),
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <DropBox/>
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/assets/style.ts
@@ -0,0 +1,8 @@
+export const component_style = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "200px",
+    width: "200px",
+    height: "50px",
+}
